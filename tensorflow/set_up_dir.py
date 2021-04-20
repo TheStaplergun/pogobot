e96211ce41a178c9bf993946a2fbc540f3229200
TheStaplergun <staplergun@gmail.com>
2021-04-20 07:30:28 -0400

    Tensor stuff

--- tensorflow/set_up_dir.py
+++ tensorflow/set_up_dir.py
@@ -1,5 +1,156 @@
 import os
 import pokemon_list
+import cv2 as cv
+from os import walk
 
-for name in pokemon_list.DEX_FOR_IMAGE_PARSE:
-  os.makedirs("training_images/{}".format(name), exist_ok=True)
+from pokemon import NATIONAL_DEX, MEGA_DEX, ALOLAN_DEX, GALARIAN_DEX, ALTERNATE_FORME_DEX
+
+relevant_dex = {
+    "national":NATIONAL_DEX,
+    "mega":MEGA_DEX,
+    "alola":ALOLAN_DEX,
+    "galar":GALARIAN_DEX,
+    "alternate":ALTERNATE_FORME_DEX
+}
+
+colors = {
+    "white":[255,255,255,255],
+    "black":[0,0,0,255],
+    "green":[126,208,136,255],
+    "blue":[243,200,83,255],
+    "pink":[179,130,236,255],
+    "orange":[66,115,255,255]
+}
+
+masks = {
+    "left-half":lambda x,y: (0, int(x/2), 0, y),
+    "right-half":lambda x,y: (int(x/2), x, 0, y),
+    "top-half":lambda x,y: (0, x, 0, int(y*0.75)),
+    "bottom-half":lambda x,y: (0, x, int(y*0.75), y)
+}
+
+
+
+def recolor_image(image, color):
+    new_image = image.copy()
+    # Get transparency mask
+    transparency_mask = new_image[:,:,3] == 0
+    # Set transparency color
+    new_image[transparency_mask] = colors.get(color)
+    # Remove alpha layer
+    new_image = cv.cvtColor(new_image, cv.COLOR_BGRA2BGR)
+    return new_image
+
+def mask_half(image, section):
+    half_image = image.copy()
+    # x is the second dimension measurement here
+    x = len(image[0])
+    y = len(image)
+    x1, x2, y1, y2 = masks.get(section)(x,y)
+    # Slice out the half we want
+    half_image = half_image[y1:y2, x1:x2]
+    return half_image
+
+# offsets = {
+#     "blank-pokemon-stats-screen.jpg":-110,
+#     "empty-catch-screen.png":0,
+#     "empty-raid-gym.png":150
+# }
+
+#def overlay_image(image, background, offset):
+
+
+def make_and_convert():
+    end_path = "./training"
+    mypath = "./images_to_move"
+    counter = 0
+
+    # helper_images = {}
+    # helper_images.update({"blank-pokemon-stats-screen.jpg":cv.imread("{}/HELPERS/blank-pokemon-stats-screen.jpg".format(mypath))})
+    # helper_images.update({"empty-catch-screen.png":cv.imread("{}/HELPERS/empty-catch-screen.png".format(mypath))})
+    # helper_images.update({"empty-raid-gym.png":cv.imread("{}/HELPERS/empty-catch-screen.png".format(mypath))})
+
+    for (dirpath, dirnames, filenames) in walk(mypath):
+        for filename in filenames:
+            if "shiny" in filenames or \
+                "pokemon" not in filename:
+                continue
+
+
+            split_file_name = filename.split('_')
+            dex_number = split_file_name[2]
+            special_category = split_file_name[3]
+            if ".png" in special_category:
+                special_category = special_category.split(".")[0]
+
+            special_dex_value = "{}_{}".format(dex_number, special_category)
+            current_file_name = "{}/{}".format(dirpath, filename)
+
+            current_image = cv.imread(current_file_name, cv.IMREAD_UNCHANGED)
+
+            image_list = {}
+            for color in colors.keys():
+                image_list[color] = recolor_image(current_image, color)
+
+            image_to_crop = image_list["white"]
+
+            for section in masks.keys():
+                image_list[section] = mask_half(image_to_crop, section)
+
+            for region, dex in relevant_dex.items():
+                if special_dex_value not in dex.keys():
+                    continue
+
+                pokemon_name = dex.get(special_dex_value)
+                dir_name = pokemon_name
+                if region == "mega":
+                    dir_name = "Mega {}".format(dir_name)
+
+                new_dir_path = "{}/{}".format(end_path, dir_name)
+                try:
+                    os.mkdir(new_dir_path)
+                except OSError as error:
+                    print("Directory [{}] already exists. Ignoring.".format(dir_name))
+
+                #os.rename(current_file_name, "{}/{}/{}".format(dirpath, dir_name, filename))
+
+
+                for modification, image in image_list.items():
+                    new_filename = "{}_{}.jpg".format(filename, modification)
+                    new_file_path = "{}/{}".format(new_dir_path, new_filename)
+
+                    cv.imwrite(new_file_path, image)
+
+
+
+            # if special_dex_value in dex.MEGA_DEX.keys():
+            #     dir_name = "Mega {}".format(dex.ALTERNATE_FORME_DEX.get(special_dex_value))
+            #     os.mkdir(dir_name)
+            #     os.rename(current_file_name, "{}/{}/{}".format(dirpath, dir_name, filename))
+            #     continue
+
+            # if special_dex_value in dex.ALOLAN_DEX.keys():
+            #     dir_name = dex.ALOLAN_DEX.get(special_dex_value)
+            #     os.mkdir(dir_name)
+            #     os.rename(current_file_name, "{}/{}/{}".format(dirpath, dir_name, filename))
+            #     continue
+
+            # if special_dex_value in dex.GALARIAN_DEX.keys():
+            #     dir_name = dex.GALARIAN_DEX.get(special_dex_value)
+            #     os.mkdir(dir_name)
+            #     os.rename(current_file_name, "{}/{}/{}".format(dirpath, dir_name, filename))
+            #     continue
+
+            # if special_dex_value in dex.ALTERNATE_FORME_DEX.keys():
+            #     dir_name = dex.ALTERNATE_FORME_DEX.get(special_dex_value)
+            #     os.mkdir(dir_name)
+            #     os.rename(current_file_name, "{}/{}/{}".format(dirpath, dir_name, filename))
+            #     continue
+
+            #counter += 1
+
+            #if counter == 2:
+                #return
+
+if __name__ == "__main__":
+    make_and_convert()
--- tensorflow/tensor.py
+++ tensorflow/tensor.py
@@ -4,18 +4,18 @@ import PIL
 import PIL.Image
 import tensorflow as tf
 from tensorflow.keras import layers
-
 import matplotlib.pyplot as plt
-#import tensorflow_datasets as tfds
 import pathlib
-#import pokemon_list
 
-data_dir = pathlib.Path("./test_training")
-labels = ['Blastoise']
+#image_count = len(list(data_dir.glob('*/*.jpg')))
+#print(image_count)
+
+# Load dataset
+data_dir = pathlib.Path("./training")
 
 batch_size = 32
-img_height = 720
-img_width = 512
+img_height = 256
+img_width = 256
 
 train_ds = tf.keras.preprocessing.image_dataset_from_directory(
     data_dir,
@@ -33,7 +33,19 @@ val_ds = tf.keras.preprocessing.image_dataset_from_directory(
     image_size=(img_height, img_width),
     batch_size=batch_size)
 
-normalization_layer = tf.keras.layers.experimental.preprocessing.Rescaling(1./255)
+class_names = train_ds.class_names
+print(class_names)
+# Visualize Data
+#plt.figure(figsize=(10, 10))
+#for images, labels in train_ds.take(1):
+#    for i in range(3):
+#        ax = plt.subplot(3, 3, i + 1)
+#        plt.imshow(images[i].numpy().astype("uint8"))
+#        plt.title(class_names[labels[i]])
+#        plt.axis("off")
+
+# Standardize the data. Reduce colors from 0-255 to 0-1
+#normalization_layer = tf.keras.layers.experimental.preprocessing.Rescaling(1./255)
 
 normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
 image_batch, labels_batch = next(iter(normalized_ds))
@@ -41,18 +53,151 @@ first_image = image_batch[0]
 # Notice the pixels values are now in `[0,1]`.
 print(np.min(first_image), np.max(first_image))
 
-class_names = train_ds.class_names
-print(class_names)
 
-plt.figure(figsize=(10, 10))
-for images, labels in train_ds.take(1):
-    for i in range(9):
-        ax = plt.subplot(3, 3, i + 1)
-        plt.imshow(images[i].numpy().astype("uint8"))
-        plt.title(class_names[labels[i]])
-        plt.axis("off")
+
+
 
 for image_batch, labels_batch in train_ds:
     print(image_batch.shape)
     print(labels_batch.shape)
     break
+
+# Configure the dataset for performance.
+AUTOTUNE = tf.data.AUTOTUNE
+
+train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+
+# Train a model
+num_classes = 3
+
+model = tf.keras.Sequential([
+  layers.experimental.preprocessing.Rescaling(1./255),
+  layers.Conv2D(32, 3, activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(32, 3, activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(32, 3, activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Flatten(),
+  layers.Dense(128, activation='relu'),
+  layers.Dense(num_classes)
+])
+
+model.compile(
+  optimizer='adam',
+  loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
+  metrics=['accuracy'])
+
+model.fit(
+  train_ds,
+  validation_data=val_ds,
+  epochs=3
+)
+
+
+# Using tf.data for finer control. Allows for setting up finer data points.
+#list_ds = tf.data.Dataset.list_files(str(data_dir/'*/*'), shuffle=False)
+#list_ds = list_ds.shuffle(image_count, reshuffle_each_iteration=False)
+
+# Split dataset into training and validation sets
+#val_size = int(image_count * 0.2)
+#train_ds = list_ds.skip(val_size)
+#val_ds = list_ds.take(val_size)
+
+
+print(tf.data.experimental.cardinality(train_ds).numpy())
+print(tf.data.experimental.cardinality(val_ds).numpy())
+
+# # Converts path to (img, label) pair
+# def get_label(file_path):
+#   # convert the path to a list of path components
+#   parts = tf.strings.split(file_path, os.path.sep)
+#   # The second to last is the class-directory
+#   one_hot = parts[-2] == class_names
+#   # Integer encode the label
+#   return tf.argmax(one_hot)
+
+# def decode_img(img):
+#   # convert the compressed string to a 3D uint8 tensor
+#   img = tf.image.decode_jpeg(img, channels=3)
+#   # resize the image to the desired size
+#   return tf.image.resize(img, [img_height, img_width])
+
+# def process_path(file_path, dummy):
+#   label = get_label(file_path)
+#   # load the raw data from the file as a string
+#   img = tf.io.read_file(file_path)
+#   img = decode_img(img)
+#   return img, label
+
+# Create dataset of image label pairs
+# Set `num_parallel_calls` so multiple images are loaded/processed in parallel.
+#train_ds = train_ds.map(process_path, num_parallel_calls=AUTOTUNE)
+#val_ds = val_ds.map(process_path, num_parallel_calls=AUTOTUNE)
+
+# Configure dataset for performance
+def configure_for_performance(ds):
+    ds = ds.cache()
+    ds = ds.shuffle(buffer_size=1000)
+    ds = ds.batch(batch_size)
+    ds = ds.prefetch(buffer_size=AUTOTUNE)
+    return ds
+
+train_ds = configure_for_performance(train_ds)
+val_ds = configure_for_performance(val_ds)
+
+# Visualize data
+image_batch, label_batch = next(iter(train_ds))
+
+IMG_SIZE = 180
+
+resize_and_rescale = tf.keras.Sequential([
+  layers.experimental.preprocessing.Resizing(IMG_SIZE, IMG_SIZE),
+  layers.experimental.preprocessing.Rescaling(1./255)
+])
+
+data_augmentation = tf.keras.Sequential([
+  layers.experimental.preprocessing.RandomFlip("horizontal_and_vertical"),
+  layers.experimental.preprocessing.RandomRotation(0.2),
+])
+
+# Adds preprocessed layers to model automatically.
+model = tf.keras.Sequential([
+  resize_and_rescale,
+  data_augmentation,
+  layers.Conv2D(16, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  # Rest of your model
+])
+
+# Manually apply processing
+#aug_ds = train_ds.map(
+#  lambda x, y: (resize_and_rescale(x, training=True), y))
+
+batch_size = 32
+AUTOTUNE = tf.data.AUTOTUNE
+
+def prepare(ds, shuffle=False, augment=False):
+  # Resize and rescale all datasets
+  ds = ds.map(lambda x, y: (resize_and_rescale(x), y),
+              num_parallel_calls=AUTOTUNE)
+
+  if shuffle:
+    ds = ds.shuffle(1000)
+
+  # Batch all datasets
+  ds = ds.batch(batch_size)
+
+  # Use data augmentation only on the training set
+  if augment:
+    ds = ds.map(lambda x, y: (data_augmentation(x, training=True), y),
+                num_parallel_calls=AUTOTUNE)
+
+  # Use buffered prefecting on all datasets
+  return ds.prefetch(buffer_size=AUTOTUNE)
+
+train_ds = prepare(train_ds, shuffle=True, augment=True)
+val_ds = prepare(val_ds)
+test_ds = prepare(test_ds)
