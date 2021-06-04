b62d3495da8f30ccc3ae7f7c052ba1232736bd73
Steven Hochheimer <steven.j.hochheimer.mil@mail.mil>
2021-06-04 15:22:36 -0400

    Move commands and events to cogs

---
+++ cogs/general_commands.py
@@ -0,0 +1,24 @@
+"""Cog containing general commands"""
+import time
+
+#import discord
+from discord.ext import commands
+
+class GeneralCommands(commands.Cog):
+    """General Commands Cog"""
+    def __init__(self, bot):
+        self.__bot = bot
+
+    @commands.command()
+    async def ping(self, ctx):
+        """Check if alive"""
+        curr = time.time()
+        latency: float = round(ctx.bot.latency * 1000.0, 2)
+        msg = await ctx.send('Pinging... 🏓')
+        await msg.edit(
+            content=f'🏓 Pong! Latency is {round((time.time() - curr) * 1000.0, 2)}ms. API latency is {latency}ms.')
+
+
+def setup(bot):
+    """Default setup function for file"""
+    bot.add_cog(GeneralCommands(bot))
