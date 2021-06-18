4e3965b988a8fbbc254ba994e66c6434d19a372e
Steven Hochheimer <steven.j.hochheimer.mil@mail.mil>
2021-06-18 17:55:28 -0400

    Remove excess comments and imports. Move task functions to own file.

--- main.py
+++ main.py
@@ -11,20 +11,12 @@ import discord
 #from discord.ext import commands
 
 import classes.bot as bot
-import classes.database as database
 import important
 #import raid_cog
-import handlers.event_handlers as EH
-import handlers.friend_code_handler as FCH
-import handlers.helpers as H
-import handlers.raid_handler as RH
-import handlers.raid_lobby_handler as RLH
-import handlers.raid_lobby_management as RLM
-import handlers.registration_handler as REGH
-import handlers.request_handler as REQH
-import handlers.startup_handler as SH
 
-DESCRIPTION = '''TheStaplergun's Pokemon Go Raid Bot'''
+from tasks import *
+
+DESCRIPTION = '''Pokemon Go Raid Bot'''
 
 #Set command_prefix to any character here.
 COMMAND_PREFIX = '-'
@@ -40,62 +32,10 @@ BOT = bot.Bot(COMMAND_PREFIX, description=DESCRIPTION, activity=GAME, intents=in
 
 BOT.pool = None
 BOT.categories_allowed = True
-#BOT.raids_enabled = True
-#BOT.bot_ready_to_process = False
-
-# async def acquire_pool_connection():
-#     """Asyncpg pool connection acquisition wrapper"""
-#     connection = await BOT.pool.acquire()
-#     return connection
-
-# async def release_pool_connection(connection):
-#     """Asyncpg pool connection release wrapper"""
-#     if connection:
-#         await BOT.pool.release(connection)
-
-# BOT.acquire = acquire_pool_connection
-# BOT.release = release_pool_connection
-
-# async def init_pool():
-#     """Set up asyncpg connection pool"""
-#     pool = await asyncpg.create_pool(database=important.DATABASE,
-#                                      port=important.PORT,
-#                                      host=important.HOST,
-#                                      user=important.DB_USER,
-#                                      password=important.PASSWORD)
-
-#     return pool
 
 live=False
-async def startup_process():
-    """Startup process. Linear process."""
-    pool = await asyncpg.create_pool(database=important.DATABASE,
-                                     port=important.PORT,
-                                     host=important.HOST,
-                                     user=important.DB_USER,
-                                     password=important.PASSWORD)
-    BOT.database = database.Database(pool)
-    await BOT.wait_until_ready()
-    #BOT.pool = await init_pool()
-    if live:
-        await SH.spin_up_message_deletions(BOT)
 
-async def status_update_loop():
-    """Updates status continually every ten minutes."""
-    await BOT.wait_until_ready()
-    await SH.start_status_update_loop(BOT)
-
-async def lobby_removal_loop():
-    """Removes lobbies as their time expires."""
-    await BOT.wait_until_ready()
-    #await RLH.establish_lobby_removal_list(BOT)
-    await SH.start_lobby_removal_loop(BOT)
-
-async def applicant_loop():
-    await BOT.wait_until_ready()
-    await SH.start_applicant_loop(BOT)
-
-def initialize_cogs(bot):
+def initialize_cogs():
     cog_list = []
     for root, _, files in os.walk("cogs"):
         for filename in files:
@@ -104,25 +44,25 @@ def initialize_cogs(bot):
                 cog_list.append(filepath.split(".py")[0].replace(os.sep, "."))
     for cog in cog_list:
         try:
-            bot.load_extension(cog)
+            BOT.load_extension(cog)
         except Exception as error:
             print(f"[!] An error occurred while loading COG [{cog}]: [{error}]")
             return False
     return True
-    
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="")
     parser.add_argument("-l", action="store_true")
     args = parser.parse_args()
 
-    if not initialize_cogs(BOT):
+    if not initialize_cogs():
         print("[!] An error occurred during cog initialization. Exiting.")
         sys.exit()
 
-    BOT.loop.create_task(startup_process())
-    BOT.loop.create_task(status_update_loop())
-    BOT.loop.create_task(applicant_loop())
-    BOT.loop.create_task(lobby_removal_loop())
+    BOT.loop.create_task(startup_process(BOT, live))
+    BOT.loop.create_task(status_update_loop(BOT))
+    BOT.loop.create_task(applicant_loop(BOT))
+    BOT.loop.create_task(lobby_removal_loop(BOT))
     if args.l:
         print("[!] Running bot live.")
         live=True
---
+++ tasks.py
@@ -0,0 +1,37 @@
+"""
+Contains async task entry task entry points.
+"""
+import asyncpg
+
+import classes.database as database
+import important
+from handlers import startup_handler as SH
+
+async def startup_process(bot, live):
+    """Startup process. Linear process."""
+    pool = await asyncpg.create_pool(database=important.DATABASE,
+                                     port=important.PORT,
+                                     host=important.HOST,
+                                     user=important.DB_USER,
+                                     password=important.PASSWORD)
+    bot.database = database.Database(pool)
+    await bot.wait_until_ready()
+    #bot.pool = await init_pool()
+    if live:
+        await SH.spin_up_message_deletions(bot)
+
+async def status_update_loop(bot):
+    """Updates status continually every ten minutes."""
+    await bot.wait_until_ready()
+    await SH.start_status_update_loop(bot)
+
+async def lobby_removal_loop(bot):
+    """Removes lobbies as their time expires."""
+    await bot.wait_until_ready()
+    #await RLH.establish_lobby_removal_list(bot)
+    await SH.start_lobby_removal_loop(bot)
+
+async def applicant_loop(bot):
+    """Processes raid applicants and adds them to raids."""
+    await bot.wait_until_ready()
+    await SH.start_applicant_loop(bot)
