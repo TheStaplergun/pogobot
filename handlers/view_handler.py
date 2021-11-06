c80350211d99d9bed31d28def0988871c8c48469
TheStaplergun <staplergun@gmail.com>
2021-11-06 16:59:02 -0400

    A

--- handlers/view_handler.py
+++ handlers/view_handler.py
@@ -11,22 +11,22 @@ class PersistentView(discord.ui.View):
 
 
     @discord.ui.button(custom_id="button_delete_raid", label="Delete", emoji="🗑️", style=discord.ButtonStyle.red)
-    async def delete_button_callback(self, interaction):
+    async def delete_button_callback(self, button: discord.ui.Button, interaction: discord.Interaction):
         await RH.remove_raid_from_button(interaction, self.__bot)
 
     @discord.ui.button(custom_id="button_sign_up_raid", label="Sign up", emoji="📝", style=discord.ButtonStyle.green)
-    async def sign_up_button_callback(self, interaction):
+    async def sign_up_button_callback(self, button: discord.ui.Button, interaction: discord.Interaction):
         await RLH.handle_application_from_button(interaction, self.__bot)
         #RH.remove_raid_from_button(interaction, bot)
 
     @discord.ui.button(custom_id="button_add_role_raid", label="Get notifications", emoji="📬")#, style=discord.ButtonStyle.green)
-    async def add_role_button_callback(self, interaction):
+    async def add_role_button_callback(self, button: discord.ui.Button, interaction: discord.Interaction):
         await interaction.channel.send("Not implemented yet")
         pass
         #RH.remove_raid_from_button(interaction, bot)
 
     @discord.ui.button(custom_id="button_remove_role_raid", label="Stop notifications", emoji="📪")#, style=discord.ButtonStyle.red)
-    async def remove_role_button_callback(self, interaction):
+    async def remove_role_button_callback(self, button: discord.ui.Button, interaction: discord.Interaction):
         await interaction.channel.send("Not implemented yet")
         pass
         #RH.remove_raid_from_button(interaction, bot)
