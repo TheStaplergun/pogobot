bb0d091bcb2fb12c7c80ddcb89487e5720c2fde5
TheStaplergun <staplergun@gmail.com>
2021-11-06 16:22:12 -0400

    Y

--- handlers/view_handler.py
+++ handlers/view_handler.py
@@ -7,22 +7,22 @@ import handlers.raid_lobby_handler as RLH
 def construct_raid_view(bot):
     raid_view = discord.ui.View(timeout=None)
 
-    @discord.ui.Button(custom_id="button_delete_raid", label="Delete", emoji="🗑️", style=discord.ButtonStyle.red)
+    @discord.ui.button(custom_id="button_delete_raid", label="Delete", emoji="🗑️", style=discord.ButtonStyle.red)
     async def delete_button_callback(interaction):
         await RH.remove_raid_from_button(interaction, bot)
 
-    @discord.ui.Button(custom_id="button_sign_up_raid", label="Sign up", emoji="📝", style=discord.ButtonStyle.green)
+    @discord.ui.button(custom_id="button_sign_up_raid", label="Sign up", emoji="📝", style=discord.ButtonStyle.green)
     async def sign_up_button_callback(interaction):
         await RLH.handle_application_from_button(interaction, bot)
         #RH.remove_raid_from_button(interaction, bot)
 
-    @discord.ui.Button(custom_id="button_add_role_raid", label="Get notifications", emoji="📬")#, style=discord.ButtonStyle.green)
+    @discord.ui.button(custom_id="button_add_role_raid", label="Get notifications", emoji="📬")#, style=discord.ButtonStyle.green)
     async def add_role_button_callback(interaction):
         await interaction.channel.send("Not implemented yet")
         pass
         #RH.remove_raid_from_button(interaction, bot)
 
-    @discord.ui.Button(custom_id="button_remove_role_raid", label="Stop notifications", emoji="📪")#, style=discord.ButtonStyle.red)
+    @discord.ui.button(custom_id="button_remove_role_raid", label="Stop notifications", emoji="📪")#, style=discord.ButtonStyle.red)
     async def remove_role_button_callback(interaction):
         await interaction.channel.send("Not implemented yet")
         pass
