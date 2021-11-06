fc0537353890f2e0c51a61837bfcc5e12db9443c
TheStaplergun <staplergun@gmail.com>
2021-11-06 18:25:40 -0400

    Fix

--- handlers/view_handler.py
+++ handlers/view_handler.py
@@ -2,6 +2,7 @@ import discord
 
 import handlers.helpers as H
 import handlers.raid_handler as RH
+import handlers.request_handler as REQH
 import handlers.raid_lobby_handler as RLH
 
 class RaidView(discord.ui.View):
@@ -21,14 +22,14 @@ class RaidView(discord.ui.View):
 
     @discord.ui.button(custom_id="button_add_role_raid", label="Get notifications", emoji="📬")#, style=discord.ButtonStyle.green)
     async def add_role_button_callback(self, button: discord.ui.Button, interaction: discord.Interaction):
-        await RH.add_request_role_to_user_from_button(interaction, self.__bot)
+        await REQH.add_request_role_to_user_from_button(interaction, self.__bot)
         #await interaction.channel.send("Not implemented yet")
         pass
         #RH.remove_raid_from_button(interaction, bot)
 
     @discord.ui.button(custom_id="button_remove_role_raid", label="Stop notifications", emoji="📪")#, style=discord.ButtonStyle.red)
     async def remove_role_button_callback(self, button: discord.ui.Button, interaction: discord.Interaction):
-        await RH.remove_request_role_from_user_from_button(interaction, self.__bot)
+        await REQH.remove_request_role_from_user_from_button(interaction, self.__bot)
         #await interaction.channel.send("Not implemented yet")
         pass
         #RH.remove_raid_from_button(interaction, bot)
