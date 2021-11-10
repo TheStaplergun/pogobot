4a6214e237eab5fd6a5c8573a00e7a29b2acf2e2
TheStaplergun <staplergun@gmail.com>
2021-11-10 02:07:45 -0500

    a

--- classes/lobby.py
+++ classes/lobby.py
@@ -13,6 +13,7 @@ class Lobby():
         self.raid_id = raid_id
         self.host = host
         self.has_filled = False
+        self.pending_unlock = False
         self.lock()
         self.five_minute_warning = False
         self.delete_time = delete_time
@@ -33,18 +34,24 @@ class Lobby():
 
     def unlock(self):
         self.has_filled = False
+        self.pending_unlock = False
         self.__bot.applicant_trigger.set()
 
     async def get_channel(self):
         self.lobby_channel = await self.__bot.retrieve_channel(self.lobby_id) if not self.lobby_channel else self.lobby_channel
 
     async def ask_to_unlock(self):
+        if self.pending_unlock:
+            return
+
         embed = discord.Embed(title="Unlock?", description="The lobby is no longer full. Would you like to gather more users?")
         await self.get_channel()
         try:
             await self.lobby_channel.send(f"<@{self.host.id}>", embed=embed, view=self.__bot.unlock_lobby_view(self.__bot))
         except discord.DiscordException:
             pass
+
+        self.pending_unlock = True
 
     async def send_five_minute_warning(self):
         embed = discord.Embed(title="Warning", description="Only five minutes remain on the time of this lobby. Would you like to extend the lobby?")
