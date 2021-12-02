958347f9f83b72e9025d3f7ad93c35d374d0376e
TheStaplergun <staplergun@gmail.com>
2021-12-02 13:25:03 -0500

    a

--- pogo_raid_lib.py
+++ pogo_raid_lib.py
@@ -95,10 +95,10 @@ def validate_and_format_message(ctx,
             embed.url = POKEBATTLER_LINK.get(embed_pokemon)
             embed.description = "Click the Pokemon name above for more in depth counter information."
         embed.set_thumbnail(url=embed_thumbnail)
-        embed.add_field(name="Tier", value=tier.lower().replace("t",""), inline=True)
+        embed.add_field(name="Tier", value=tier.lower().replace("t",""), inline=False)
         embed.add_field(name="Weather", value=embed_weather, inline=True)
         embed.add_field(name="Applicants", value=0, inline=False)
-        embed.add_field(name="Members/Slots", value=f"0/{embed_invites}", inline=False)
+        embed.add_field(name="Members/Slots", value=f"0/{embed_invites}", inline=True)
         #embed.set_footer(text="To join this raid, DM the host above.")
         if embed_pokemon in RAID_COUNTER_GUIDE:
             embed.set_image(url=RAID_COUNTER_GUIDE.get(embed_pokemon))
