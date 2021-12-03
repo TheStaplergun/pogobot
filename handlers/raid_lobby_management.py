bd4fb096b1e68e19362486cae338954172de8a93
TheStaplergun <staplergun@gmail.com>
2021-12-02 19:04:04 -0500

    a

--- handlers/raid_lobby_management.py
+++ handlers/raid_lobby_management.py
@@ -67,6 +67,7 @@ async def extend_duration_of_lobby(bot, ctx):
         await RLH.remove_lobby_by_lobby_id(bot, lobby_data.get("lobby_channel_id"))
         return
 
+
     max_remaining_extendable_time = 2700 - old_total_duration.total_seconds()
     max_remaining_extendable_time_type = "minute"
     #new_time_extension = 0
@@ -98,11 +99,12 @@ async def extend_duration_of_lobby(bot, ctx):
 
     time_until_expiration_as_minutes = math.ceil((new_delete_time - datetime.now()).total_seconds()/60)
     new_embed = discord.Embed(title="System Notification", description=f"The host has extended the lobby duration by {extension_amount} {extension_measurement}. It will now expire in {time_until_expiration_as_minutes} minutes.\n\nYou can add up to {max_remaining_extendable_time} more {max_remaining_extendable_time_type}")
-    view = bot.extend_lobby_view(bot) if max_remaining_extendable_time > 1 else None
-    await RLH.update_raid_removal_and_lobby_removal_times(bot, lobby_data.get("raid_message_id"), time_to_remove=new_delete_time, lobby_id=lobby_data.get("lobby_channel_id"), view=view)
+    print("Sending updated time")
+    await RLH.update_raid_removal_and_lobby_removal_times(bot, lobby_data.get("raid_message_id"), time_to_remove=new_delete_time, lobby_id=lobby.id)
 
+    view = bot.extend_lobby_view(bot) if max_remaining_extendable_time > 1 else None
 #    await RLH.update_delete_time_with_given_time(bot, new_delete_time, lobby_data.get("raid_message_id"))
-    await lobby.send(embed=new_embed)
+    await lobby.send(embed=new_embed, view=view)
 
 async def host_manual_remove_lobby(bot, ctx):
     lobby_data = await RLH.get_lobby_data_by_user_id(bot, ctx.user_id)
